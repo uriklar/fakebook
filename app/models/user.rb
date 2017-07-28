6dcc7005dee7a0b58e98038bf160a4b5a46697c6
Uri Klar <uri.k@phytech.com>
2017-07-28 18:16:40 +0300

    Added people you may know and friend lists

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,14 @@ class User < ActiveRecord::Base
 	has_many :posts
 	has_many :friendships
 	has_many :friends, through: :friendships
+	has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
+	has_many :inverse_friends, :through => :inverse_friendships, :source => :user
+
+	def people_you_may_know
+		User.all - [self] - friends - inverse_friends
+	end
+
+	def all_friends
+		friends + inverse_friends
+	end
 end
